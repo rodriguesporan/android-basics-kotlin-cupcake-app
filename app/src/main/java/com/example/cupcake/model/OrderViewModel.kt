c9630f99c3f3d032bdc01259d420e2c96c1dd5ea
rodriguesporan <rodriguesporan@gmail.com>
2021-03-12 17:01:15 -0300

    feat: using viewModel with databinding onto the fragments

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,8 +3,14 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel: ViewModel() {
+
+//    init {
+//        resetOrder()
+//    }
 
     private val _quantity = MutableLiveData<Int>(0)
     val quantity: LiveData<Int> get() = _quantity
@@ -18,6 +24,21 @@ class OrderViewModel: ViewModel() {
     private val _date = MutableLiveData<String>("")
     val date: LiveData<String> get() = _date
 
+    val dateOptions = getPickupOptions()
+
+    private fun getPickupOptions(): List<String> {
+        var options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+
+        return options
+    }
+
     fun setQuantity(quantityCupcakes: Int) {
         _quantity.value = quantityCupcakes
     }
@@ -29,4 +50,13 @@ class OrderViewModel: ViewModel() {
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
     }
+
+    fun hasNoFlavorSet(): Boolean = _flavor.value.isNullOrEmpty()
+
+//    private fun resetOrder() {
+//        _quantity.value = 0
+//        _flavor.value = ""
+//        _date.value = dateOptions[0]
+//        _price.value = 0.0
+//    }
 }
